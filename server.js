0f107d271978f17b8cfe2a9d3ad02c73b554daaa
Andrey Kulakovskiy <kylak.a@tut.by>
2024-01-08 00:06:14 +0300

    sprint_3, fixed server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static('./dist'));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve('./dist/index.html'));
+});
 
 app.listen(PORT, function log() {
   console.log(`Example app listening on port ${PORT}!`);
